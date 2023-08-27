517194485a886fb8ec435bff54c809469ebafce7
Ahmedshafea <ahmedshafea@outlook.com>
2023-08-27 18:32:22 +0300

    Add Teamplate and Static Files

--- books/views.py
+++ books/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
+def index(request):
+    
+
+    return  render(request, "pages/index.html")
+
+def books(request):
+    
+    return  render(request, "pages/books.html")
+
+def delete(request,id):
+
+    return  render(request, "pages/delete.html")
+
+def update(request,id):
+
+    return  render(request, "pages/update.html")
+
+
+
 # Create your views here.
